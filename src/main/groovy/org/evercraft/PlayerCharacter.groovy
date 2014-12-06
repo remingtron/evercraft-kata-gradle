272b512702ef6399772f0349b457112d0c8d5154
Remington Davenport <remington.davenport@gmail.com>
2014-12-05 19:31:22 -0500

    adding ability to attack

--- src/main/groovy/org/evercraft/PlayerCharacter.groovy
+++ src/main/groovy/org/evercraft/PlayerCharacter.groovy
@@ -8,13 +8,17 @@ enum Alignment {
 
 class PlayerCharacter {
 
-    String name
-    Alignment alignment
+    final String name
+    final Alignment alignment
     int armorClass = 10
     int hitPoints = 5
 
     def PlayerCharacter(String name, Alignment alignment = Alignment.GOOD) {
         this.name = name
         this.alignment = alignment
+    }
+
+    def attack(PlayerCharacter opponent, int roll) {
+        if (roll >= opponent.armorClass) {opponent.hitPoints -= 1}
     }
 }
--- src/test/groovy/org/evercraft/PlayerCharacterTest.groovy
+++ src/test/groovy/org/evercraft/PlayerCharacterTest.groovy
@@ -5,11 +5,12 @@ import org.junit.Test
 
 class PlayerCharacterTest {
 
-    def arbitraryCharacter;
+    def arbitraryCharacter, characterToAttack;
 
     @Before
     public void setup() {
         arbitraryCharacter = new PlayerCharacter("bozo")
+        characterToAttack = new PlayerCharacter("victim")
     }
 
     @Test
@@ -44,5 +45,23 @@ class PlayerCharacterTest {
     @Test
     public void itHasFiveHitPointsByDefault() {
         assert arbitraryCharacter.hitPoints == 5
+    }
+
+    @Test
+    public void itDealsDamageToOtherCharacterIfRollIsGreaterThanOpponentArmorClass() {
+        arbitraryCharacter.attack(characterToAttack, 11)
+        assert characterToAttack.hitPoints == 4
+    }
+
+    @Test
+    public void itDoesNotDealDamageToOtherCharacterIfRollIsLessThanOpponentArmorClass() {
+        arbitraryCharacter.attack(characterToAttack, 9)
+        assert characterToAttack.hitPoints == 5
+    }
+
+    @Test
+    public void itDealsDamageToOtherCharacterIfRollIsEqualToOpponentArmorClass() {
+        arbitraryCharacter.attack(characterToAttack, 10)
+        assert characterToAttack.hitPoints == 4
     }
 }
